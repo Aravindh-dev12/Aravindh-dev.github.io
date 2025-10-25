9a3feba64db662e9b0e9d61f2c93cc961ab952c5
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2025-10-25 14:36:34 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -39,4 +39,31 @@
     $("body").scrollspy({
         target: "#sideNav",
     });
+
+    // ------------------------------------------------------------------
+    // Ensure page opens on About (if no hash or if hash is #projects)
+    // ------------------------------------------------------------------
+    // Behavior:
+    //  - If the URL has no hash (user opens site root) OR if the URL hash is "#projects",
+    //    jump immediately to the element with id="about" and replace the URL hash with #about
+    //  - If there's another hash (e.g., #contact or #projectX), we leave it alone
+    // ------------------------------------------------------------------
+    $(function () {
+        var currentHash = location.hash;
+        // if there is no hash, or hash explicitly points to #projects, force About
+        if (!currentHash || currentHash === "#projects") {
+            var about = $("#about");
+            if (about.length) {
+                // instant scroll to About (0ms so user doesn't see animated jump)
+                $('html, body').stop().animate({ scrollTop: about.offset().top }, 0);
+                // update URL to #about without adding a new history entry
+                try {
+                    history.replaceState(null, null, "#about");
+                } catch (e) {
+                    // ignore if history API not supported
+                }
+            }
+        }
+    });
+
 })(jQuery); // End of use strict
